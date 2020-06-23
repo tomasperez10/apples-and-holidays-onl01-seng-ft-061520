e319ebf33ec646c40d75e562191f676eaeecf273
Tomas Perez <tomasitopg6@gmail.com>
2020-06-23 00:20:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   
   holiday_hash.each do |season, holiday|
-    puts "#{season.capitalize}:"
+    puts " #{season.capitalize}:"
     
     holiday.each do |holiday, supply|
       puts "#{holiday.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{supply.join(", ")}"
