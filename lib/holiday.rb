f3dd8551b978f9369817cf409f9363f5dccd43be
Tomas Perez <tomasitopg6@gmail.com>
2020-06-23 00:22:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,7 +81,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   
-  holiday_hash
+  holiday_hash.map do |
 
 end
 
