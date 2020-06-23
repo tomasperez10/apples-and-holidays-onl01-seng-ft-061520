9640d4b4fb9b08cebf282a1aaf6cc495c11c7bb8
Tomas Perez <tomasitopg6@gmail.com>
2020-06-23 00:23:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,7 +83,7 @@ def all_holidays_with_bbq(holiday_hash)
   
   holiday_hash.map do |season, holiday|
     holiday.map do |holiday, supply|
-      holiday 
+      holiday if supply. include 
 
 end
 
